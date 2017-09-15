11ba64cdc50520b414b145f1bed9e9ff86a5d1c0
GoodLuckDay <haon4658@gmail.com>
2017-09-15 20:26:12 +0900

    data upload and insertion and mergesort implement

--- hw01/src/Insertion_sorting.java
+++ hw01/src/Insertion_sorting.java
@@ -1,5 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Insertion_sorting {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    static final int file_size = 10000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("./Random/"+file_size+".txt"));
+        int[] array = new int[file_size];
+        int index = 0;
+        String line = null;
+        while((line = bufferedReader.readLine()) != null){
+            StringTokenizer stringTokenizer = new StringTokenizer(line);
+            while(stringTokenizer.hasMoreTokens()){
+                array[index] = Integer.parseInt(stringTokenizer.nextToken());
+                index++;
+            }
+        }
+
+        long start = System.currentTimeMillis();
+        for(int i=1; i<file_size; i++){
+            int key = array[i];
+            int j = i-1;
+            while(j >= 0 && array[j] > key){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("실행시간 : "+(end - start) / 1000.0 +"초");
+
     }
 }
--- hw01/src/merge_sorting.java
+++ hw01/src/merge_sorting.java
@@ -1,5 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class merge_sorting {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    static final int file_size = 10000000;
+    static int[] array = new int[file_size];
+    static int[] helper = new int[file_size];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("./Random/"+file_size+".txt"));
+        int index = 0;
+        String line = null;
+        while((line = bufferedReader.readLine()) != null){
+            StringTokenizer stringTokenizer = new StringTokenizer(line);
+            while(stringTokenizer.hasMoreTokens()){
+                array[index] = Integer.parseInt(stringTokenizer.nextToken());
+                index++;
+            }
+        }
+        long start = System.currentTimeMillis();
+        mergeSort(0, file_size-1);
+        long end = System.currentTimeMillis();
+        System.out.println("실행시간 : "+(end - start) / 1000.0 +"초");
     }
+
+    public static void mergeSort(int left, int right){
+        if(left < right){
+            int mid = (left + right) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid+1, right);
+            merge(left, mid, right);
+        }
+    }
+
+    private static void merge(int left, int mid, int right) {
+        for(int i=left; i<=right; i++){
+            helper[i] = array[i];
+        }
+        int i = left;
+        int j = mid+1;
+        int k = left;
+
+        while(i <= mid && j <=right){
+            if(helper[i] < helper[j]){
+                array[k] = helper[i];
+                i++;
+            }
+            else{
+                array[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i <= mid){
+            array[k] = helper[i];
+            i++;
+            k++;
+        }
+
+    }
+
+
 }
