7f9245b30df5ee002acebdacdb4cbd3df81e68af
Goodluckdays <haon4658@gmail.com>
2017-09-24 17:29:14 +0900

    hw02 commit

--- hw02/src/QuickSort.java
+++ hw02/src/QuickSort.java
@@ -1,5 +1,62 @@
-public class QuickSort {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class QuickSort {
+    static final int file_size = 50000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("./Worst/"+file_size+".txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("./201302482_quick"));
+        int[] array = new int[file_size];
+        int count = 0;
+        String str = null;
+
+        while((str = bufferedReader.readLine()) != null){
+            StringTokenizer stringTokenizer = new StringTokenizer(str);
+            while(stringTokenizer.hasMoreTokens()){
+                array[count++] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+
+        quickSort(array, 0, file_size-1);
+
+        for(int i=0; i<file_size-1; i++){
+            bufferedWriter.write(array[i]+" ");
+        }
+        bufferedWriter.write(array[file_size-1]+"");
+        bufferedWriter.flush();
+    }
+
+    public static void quickSort(int[] A, int left, int right){
+        if(left <= right){
+            int pivot = partition(A,left,right);
+            quickSort(A, left, pivot-1);
+            quickSort(A, pivot+1, right);
+        }
+    }
+
+    public static int partition(int[] A, int left, int right){
+        int pivot = A[right];
+        int i = left;
+        int j = right-1;
+        while(j != i-1){
+            if(pivot >= A[i])
+                i++;
+            else if(pivot < A[j])
+                j--;
+            else{
+                swap(A, i, j);
+                i++;
+                j--;
+            }
+        }
+        swap(A, i, right);
+        return i;
+    }
+
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
