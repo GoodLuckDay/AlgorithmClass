d11d9f3ace2772aee22c709cbfc5968a5937b3d8
Goodluckdays <haon4658@gmail.com>
2017-10-18 18:37:14 +0900

    hw04 karatsuba refactoring digitCount Method

--- hw04/src/Karatsuba.java
+++ hw04/src/Karatsuba.java
@@ -6,20 +6,21 @@ import java.nio.file.Paths;
 
 public class Karatsuba {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(Paths.get("./dataset04/data05_karatsuba.txt").toString()));
-        BigInteger A = new BigInteger(bufferedReader.readLine());
-        BigInteger B = new BigInteger(bufferedReader.readLine());
+//        BufferedReader bufferedReader = new BufferedReader(new FileReader(Paths.get("./dataset04/data05_karatsuba.txt").toString()));
+//        BigInteger A = new BigInteger(bufferedReader.readLine());
+//        BigInteger B = new BigInteger(bufferedReader.readLine());
+        BigInteger A = new BigInteger("12345678");
+        BigInteger B = new BigInteger("65432187");
         System.out.println(calculate_karatsuba(A, B));
     }
 
     private static BigInteger calculate_karatsuba(BigInteger A, BigInteger B) {
-        int m = digitCount(A)/2;
+        int m = String.valueOf(A.longValue()).length()/2;
         if(m == 1){
             return A.multiply(B);
         }
         BigInteger x1 = A.divide(new BigInteger(String.valueOf((long)Math.pow(10,m))));
         BigInteger x2 = A.mod(new BigInteger(String.valueOf((long)Math.pow(10,m))));
-        m = digitCount(B)/2;
         BigInteger y1 = B.divide(new BigInteger(String.valueOf((long)Math.pow(10,m))));
         BigInteger y2 = B.mod(new BigInteger(String.valueOf((long)Math.pow(10,m))));
 
@@ -30,14 +31,12 @@ public class Karatsuba {
         return z2.multiply(new BigInteger(String.valueOf((long)Math.pow(10,2*m)))).add(z1.multiply(new BigInteger(String.valueOf((long)Math.pow(10,m))))).add(z0);
     }
 
-    private static int digitCount(BigInteger number) {
-        int count = 0;
-        while(number.intValue() != 0){
-            number = number.divide(new BigInteger("10"));
-            count++;
-        }
-        return count;
-    }
-
-
+//    private static int digitCount(BigInteger number) {
+//        int count = 0;
+//        while(number.intValue() != 0){
+//            number = number.divide(new BigInteger("10"));
+//            count++;
+//        }
+//        return count;
+//    }
 }
