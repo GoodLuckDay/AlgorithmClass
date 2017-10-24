a695b5c5e5bfc9cf3fcc09b67e7bd4e5733e35c0
GoodLuckDay <haon4658@gmail.com>
2017-10-25 01:48:13 +0900

    MinimizingLateness priority Queue removed

--- hw05/src/minimizingLateness.java
+++ hw05/src/minimizingLateness.java
@@ -2,44 +2,23 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
 
 public class minimizingLateness{
     public static void main(String[] args) throws IOException {
-        PriorityQueue<Data> priorityQueue = new PriorityQueue<>(new Comparator<Data>() {
-            @Override
-            public int compare(Data o1, Data o2) {
-                if(o1.d < o2.d){
-                    return -1;
-                }
-                else if(o1.d > o2.d){
-                    return 1;
-                }
-                else{
-                    if(o1.t < o2.t){
-                        return -1;
-                    }
-                    else if(o1.t > o2.t){
-                        return 1;
-                    }
-                    else{
-                        return 0;
-                    }
-                }
-            }
-        });
+
         BufferedReader bufferedReader = new BufferedReader(new FileReader(Paths.get("./data06_lateness.txt").toString()));
+        ArrayList<Data> arrayList = new ArrayList<>();
         int N = Integer.parseInt(bufferedReader.readLine());
         for(int i=0; i<N; i++){
             String[] tempStr = bufferedReader.readLine().split(" ");
-            priorityQueue.add(new Data(Integer.parseInt(tempStr[0]), Integer.parseInt(tempStr[1])));
+            arrayList.add(new Data(Integer.parseInt(tempStr[0]), Integer.parseInt(tempStr[1])));
         }
 
         int lateness = 0;
         int time = 0;
-        while(!priorityQueue.isEmpty()) {
-            Data d = priorityQueue.poll();
+        for(int i=0; i<arrayList.size(); i++){
+            Data d = arrayList.get(i);
             int tempF = time + d.t;
             time = time + d.t;
             lateness += Math.max(0, tempF - d.d);
