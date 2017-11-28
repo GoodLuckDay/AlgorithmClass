f31bbc0e5201fbf254e5c7e861a871c3dfe7e80b
GoodLuckDay <haon4658@gmail.com>
2017-11-28 15:39:25 +0900

    SegmentAlignment error modify

--- hw09/src/SegmentAlignment.java
+++ hw09/src/SegmentAlignment.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class SegmentAlignment {
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
     static HashMap<Integer, Integer> arrowPath = new HashMap<Integer, Integer>();
     static final int GAP = 1;
     static final int MISMATCH = 2;
@@ -18,8 +21,8 @@ public class SegmentAlignment {
 //        align(sentence1, sentence2);
 //        String y = "ocurrance";
 //        String x = "occurrence";
-        String y = "CTGACCTACCT";
-        String x = "CCTGACTACAT";
+        String y = "CAGTTGCAA";
+        String x = "AAGGTATGAATC";
         align(x, y, 0, 0);
         for (Integer integer : arrowPath.keySet()) {
             System.out.println(integer + " " + arrowPath.get(integer));
@@ -35,27 +38,26 @@ public class SegmentAlignment {
             backTracking(xStart, yStart, n, m);
             return;
         }
-
-        int[] YPrefix = AllYPrefixCosts(sentence1, n / 2, sentence2, xStart, yStart);
-        int[] YSuffix = AllSuffixCosts(sentence1, n / 2, sentence2, xStart, yStart);
+        int[] YPrefix = AllYPrefixCosts(sentence1, n / 2, sentence2);
+        int[] YSuffix = AllSuffixCosts(sentence1, n / 2, sentence2);
 
         int cost = Integer.MAX_VALUE;
         int best = Integer.MAX_VALUE;
         int bestq = 0;
         for (int q = 0; q < m; q++) {
             cost = YPrefix[q] + YSuffix[q];
-            if (cost < best) {
+            if (cost <= best) {
                 best = cost;
                 bestq = q;
             }
         }
 
         arrowPath.put(n / 2 + xStart, bestq + yStart);
-        align(sentence1.substring(0, n / 2), sentence2.substring(0, bestq), xStart,  yStart);
+        align(sentence1.substring(0, n / 2), sentence2.substring(0, bestq), xStart, yStart);
         align(sentence1.substring(n / 2, n), sentence2.substring(bestq, m), xStart + n / 2, yStart + bestq);
     }
 
-    private static int[] AllSuffixCosts(String sentence1, int mid, String sentence2, int xStart, int yStart) {
+    private static int[] AllSuffixCosts(String sentence1, int mid, String sentence2) {
         String newSentence = "";
         for (int i = sentence1.length() - 1; i >= mid; i--) {
             newSentence += sentence1.charAt(i);
@@ -73,7 +75,7 @@ public class SegmentAlignment {
         return temp;
     }
 
-    private static int[] AllYPrefixCosts(String sentence1, int mid, String sentence2, int xStart, int yStart) {
+    private static int[] AllYPrefixCosts(String sentence1, int mid, String sentence2) {
         standardAlignment(sentence1.substring(0, mid), sentence2);
         return array[mid];
     }
@@ -98,17 +100,17 @@ public class SegmentAlignment {
 
     private static void backTracking(int xStart, int yStart, int n, int m) {
         int i = n, j = m;
-        while(i != 0 && j != 0) {
+        while (i != 0 || j != 0) {
             arrowPath.put(xStart+i, yStart+j);
-            if(i == 0){
+            if (i == 0) {
                 j--;
                 continue;
             }
-            if(j == 0){
+            if (j == 0) {
                 i--;
                 continue;
             }
-            if (array[i- 1][j] < array[i][j]) {
+            if (array[i - 1][j] < array[i][j]) {
                 i = i - 1;
                 continue;
             }
@@ -122,9 +124,11 @@ public class SegmentAlignment {
 
             }
         }
+        arrowPath.put(xStart+i, yStart+j);
     }
 
     private static int calculateMisMisMatch(String st1, String st2, int i, int j) {
         return st1.charAt(i - 1) == st2.charAt(j - 1) ? 0 : MISMATCH;
     }
 }
+
